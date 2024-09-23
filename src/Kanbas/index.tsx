fb7efaebd47167487c83d0177d0b692cccc380a5
Darren Chen <Darrenchen813@gmail.com>
2024-09-23 00:20:20 -0400

    Done with 3.2.4

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,7 +1,14 @@
+import Account from "./Account"
+import { Routes, Route, Navigate } from "react-router";
+
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
       <h1>Kanbas</h1>
+      <Routes>
+        <Route path="/" element={<Navigate to="Account" />} />
+        <Route path="/Account/*" element={<Account />} />
+      </Routes>
     </div>
   );
 }
