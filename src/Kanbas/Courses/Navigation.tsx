0834571ed5a51b13eb4b6e1d97dbb0260682b302
Darren Chen <Darrenchen813@gmail.com>
2024-09-23 19:22:22 -0400

    Done with 3.6

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -15,7 +15,6 @@ export default function CoursesNavigation() {
             </Link><br/>
             <Link id="wd-course-grades-link" to="/Kanbas/Courses/1234/Grades">Grades</Link><br/>
             <Link id="wd-course-people-link" to="/Kanbas/People">People</Link><br/> 
-            {/* TODO: Make this `/Kanbas/Courses/1234/People`? */}
         </div>
     )
 }
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Modules from "./Modules";
+import Home from "./Home";
 import CoursesNavigation from "./Navigation";
 import { Navigate, Route, Routes } from "react-router";
 
@@ -16,7 +17,7 @@ export default function Courses() {
           <td valign="top">
             <Routes>
               <Route path="/" element={<Navigate to="Home" />} />
-              <Route path="Home" element={<h2>Home</h2>} />
+              <Route path="Home" element={<Home />} />
               <Route path="Modules" element={<Modules />} />
               <Route path="Assignments" element={<h2>Assignments</h2>} />
               <Route
