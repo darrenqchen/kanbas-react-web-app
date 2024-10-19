f5446df508f59df9fb68d6a635f4c8434300569b
Darren Chen <Darrenchen813@gmail.com>
2024-10-19 19:09:09 -0400

    a3 Done with 3.4

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,37 +1,40 @@
+import {courses} from "../Database";
 import React from "react";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import Modules from "./Modules";
 import Home from "./Home";
 import CoursesNavigation from "./Navigation";
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes, useParams } from "react-router";
 import PeopleTable from "./People/Table";
 import { HiOutlineBars3 } from "react-icons/hi2";
 import { FaAlignJustify } from "react-icons/fa";
 
 export default function Courses() {
-  return (
-    <div id="wd-courses">
-      <h2 className="text-danger">
-        <FaAlignJustify className="me-4 fs-4 mb-1" />
-        Course 1234{" "}
-      </h2>{" "}
-      <hr />
-      <div className="d-flex">
-        <div className="d-none d-md-block">
-          <CoursesNavigation />
+    const { cid } = useParams();
+    const course = courses.find((course) => course._id === cid);
+    return (
+        <div id="wd-courses">
+            <h2 className="text-danger">
+                <FaAlignJustify className="me-4 fs-4 mb-1" />
+                {course && course.name}
+            </h2>
+            <hr />
+            <div className="d-flex">
+                <div className="d-none d-md-block">
+                    <CoursesNavigation />
+                </div>
+                <div className="flex-fill">
+                    <Routes>
+                        <Route path="/" element={<Navigate to="Home" />} />
+                        <Route path="Home" element={<Home />} />
+                        <Route path="Modules" element={<Modules />} />
+                        <Route path="Assignments" element={<Assignments />} />
+                        <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+                        <Route path="People" element={<PeopleTable />} />
+                    </Routes>
+                </div>
+            </div>
         </div>
-        <div className="flex-fill">
-          <Routes>
-            <Route path="/" element={<Navigate to="Home" />} />
-            <Route path="Home" element={<Home />} />
-            <Route path="Modules" element={<Modules />} />
-            <Route path="Assignments" element={<Assignments />} />
-            <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-            <Route path="People" element={<PeopleTable />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
