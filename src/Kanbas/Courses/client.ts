fb843d6c1a869f2ecdd32f885dd62b7d8cdcb7d3
Darren <darrenchen813@gmail.com>
2024-11-22 14:48:50 -0500

    a5 Done with 4.5

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+// Courses
 export const fetchAllCourses = async () => {
     const { data } = await axios.get(COURSES_API);
     return data;
@@ -12,4 +13,13 @@ export const deleteCourse = async (id: string) => {
 export const updateCourse = async (course: any) => {
     const { data } = await axios.put(`${COURSES_API}/${course._id}`, course);
     return data;
+};
+// Modules
+export const findModulesForCourse = async (courseId: string) => {
+    const response = await axios.get(`${COURSES_API}/${courseId}/modules`);
+    return response.data;
+};
+export const createModuleForCourse = async (courseId: string, module: any) => {
+    const response = await axios.post(`${COURSES_API}/${courseId}/modules`, module);
+    return response.data;
 };
